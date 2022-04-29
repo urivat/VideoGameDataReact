709b660c0e2c246b9178af061793b50836131157
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-04-29 11:46:36 -0500

    Completed Search Bar Functionality

--- videogamesdata/src/components/NavBar/NavBar.jsx
+++ videogamesdata/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,16 @@
 import SearchBar from "../SearchBar/SearchBar";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <div className="navbar">
       <h1>Global Game Data</h1>
-      
+      <SearchBar
+        placeholder="search"
+        searchTerm={props.searchTerm}
+        setSearchTerm={props.setSearchTerm}
+        data={props.videoGames}
+        handleSearchTerm={props.handleSearchTerm}
+      />
     </div>
   );
 };
